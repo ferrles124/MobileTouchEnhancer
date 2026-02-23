12b20f723d7f3b09fb9d4143eb4a469f4d410a57
ferrles124 <mehmetemredursun9@gmail.com>
2026-02-23 23:16:31 +0300

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,19 +1,33 @@
 using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewValley;
 
 namespace MobileTouchEnhancer
 {
     public class ModEntry : Mod
     {
+        private ModConfig Config = null!;
         private DateTime _pressStartTime;
         private DateTime _lastClickTime;
         private bool _isTracking = false;
+        private Vector2 _circlePos = Vector2.Zero;
 
         public override void Entry(IModHelper helper)
         {
+            this.Config = helper.ReadConfig<ModConfig>();
             helper.Events.Input.ButtonPressed += OnButtonPressed;
             helper.Events.Input.ButtonReleased += OnButtonReleased;
+            helper.Events.Input.CursorMoved += (s, e) => _circlePos = e.NewPosition.GetScreenPixels();
+            helper.Events.Display.RenderGuiStep += OnRenderGuiStep;
+        }
+
+        private void OnRenderGuiStep(object? sender, RenderGuiStepEventArgs e)
+        {
+            // İmlere yardımcı bir daire/işaretçi çiz
+            e.SpriteBatch.Draw(Game1.mouseCursors, _circlePos, new Rectangle(0, 0, 64, 64), Color.White * 0.7f, 0f, new Vector2(32, 32), 1f, SpriteEffects.None, 1f);
         }
 
         private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
@@ -31,13 +45,22 @@ namespace MobileTouchEnhancer
             {
                 _isTracking = false;
                 var now = DateTime.Now;
-                var holdDuration = (now - _pressStartTime).TotalMilliseconds;
+                var duration = (now - _pressStartTime).TotalMilliseconds;
 
-                if (holdDuration >= 500) { this.Helper.Input.TriggerPress(SButton.F); }
-                else {
-                    var timeSinceLastClick = (now - _lastClickTime).TotalMilliseconds;
-                    if (timeSinceLastClick <= 300) { this.Helper.Input.TriggerPress(SButton.MouseLeft); }
-                    else { this.Helper.Input.TriggerPress(SButton.MouseRight); }
+                if (duration >= Config.LongPressMs)
+                {
+                    this.Helper.Input.TriggerPress(Config.LongPressButton);
+                }
+                else
+                {
+                    if ((now - _lastClickTime).TotalMilliseconds <= Config.DoubleTapMs)
+                    {
+                        this.Helper.Input.TriggerPress(Config.DoubleTapButton);
+                    }
+                    else
+                    {
+                        this.Helper.Input.TriggerPress(Config.SingleTapButton);
+                    }
                 }
                 _lastClickTime = now;
             }
